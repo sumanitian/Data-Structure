c79b27b1cc16de0c7940213d1d33d2255563d4c1
sumanitian <sumanprasad26042001@gmail.com>
2024-10-17 12:09:54 +0530

    insertion sort added

---
+++ InsertionSort.cpp
@@ -0,0 +1,30 @@
+#include"iostream"
+using namespace std;
+
+int main(){
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    cout<<"Enter the element in array: ";
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+    for(int i=1;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
+                //for gfg
+                // int temp = arr[j];
+                // arr[j] = arr[j-1];
+                // arr[j-1] = temp;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
